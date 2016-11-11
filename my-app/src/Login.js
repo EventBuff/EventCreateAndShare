68cb53b14b0c3526dfeef1dbbe578d60bc09c3cd
Lich Amnesia <alwaysxiaop@gmail.com>
2016-11-11 11:50:56 -0700

    add sign up

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -10,11 +10,12 @@ import React, { Component } from 'react';
 import './Login.css';
 // import Navigation from './Navigation';
 import axios from 'axios';
-import { Button, Col, ControlLabel, Form, FormGroup, FormControl, Checkbox}
+import { OverlayTrigger, Tooltip, Popover, Modal, Button, Col, ControlLabel, Form, FormGroup, FormControl, Checkbox}
   from 'react-bootstrap';
 // import { Link } from 'react-router';
 
 class Login extends Component {
+
   constructor(props) {
     super(props);
 
@@ -22,18 +23,29 @@ class Login extends Component {
       email: '111@222.com',
       password: '66788',
       posts: '',
-      postsCheckNum: 0
+      postsCheckNum: 0,
+      showModal: false
     };
     this.showProfile = this.showProfile.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
 
   componentDidMount() {
     // const slug = this.props.params.slug;
     // axios.get('/profile', {
     //   params: {
-    //     userid: slug  
+    //     userid: slug
     //   }
     //   }).then(res => {
     //     this.showProfile(res.data);
@@ -43,7 +55,7 @@ class Login extends Component {
   showProfile(data) {
     console.log(data);
     if (data === null || data.length === 0) {
-      this.setState({ 
+      this.setState({
           posts: ''
       });
     } else {
@@ -71,9 +83,73 @@ class Login extends Component {
 
   }
 
+  handleSubmit(){
+    console.log("click sign up");
+  }
+
   render() {
-    var formInstance = 
-    this.state.postsCheckNum 
+    function FieldGroup({ id, label, help, ...props }) {
+      return (
+        <FormGroup controlId={id}>
+          <ControlLabel>{label}</ControlLabel>
+          <FormControl {...props} />
+          {help && <HelpBlock>{help}</HelpBlock>}
+        </FormGroup>
+      );
+    }
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    );
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    );
+    var signContent =
+      <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sign up</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={this.handleSubmit}>
+              <FieldGroup
+                id="formControlsEmail"
+                type="email"
+                label="Email Address"
+                placeholder="Enter Email"
+              />
+              <FieldGroup
+                id="formControlsUserName"
+                type="text"
+                label="User Name"
+                placeholder="User Name"
+              />
+              <FieldGroup
+                id="formControlsPassword"
+                type="Password"
+                label="Password"
+                placeholder="Enter Password"
+              />
+              <FieldGroup
+                id="formControlsRePassword"
+                type="Password"
+                label="Re Password"
+                placeholder="ReEnter Password"
+              />
+              <Button type="submit">
+                Submit
+              </Button>
+            </form>
+            </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
+    var formInstance =
+    this.state.postsCheckNum
       ? this.state.posts
       :
   <Form horizontal>
@@ -106,8 +182,10 @@ class Login extends Component {
         <Button onClick={this.handleClick}>
           log in
         </Button>
-        <Button onClick={this.handleClick}>
-          Sign in
+        <Button
+          onClick={this.open}
+        >
+          Sign Up
         </Button>
       </Col>
     </FormGroup>
@@ -116,6 +194,7 @@ class Login extends Component {
     return (
       <div>
       {formInstance}
+      {signContent}
       </div>
     );
   }
