66936f83b78d57623459413fb5e601de07add111
YanLi26 <yali2241@colorado.edu>
2016-11-12 22:25:42 -0700

    delete equipment controller

--- BackEnd/src/main/java/com/BackEnd/controller/DeleteEquipmentController.java
+++ BackEnd/src/main/java/com/BackEnd/controller/DeleteEquipmentController.java
@@ -1,7 +1,28 @@
 package com.BackEnd.controller;
 
+import com.BackEnd.domain.EquipmentRepository;
+import com.BackEnd.domain.EventEquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by yanli on 11/11/16.
  */
+@RestController
 public class DeleteEquipmentController {
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+    @Autowired
+    private EventEquipmentRepository eventEquipmentRepository;
+
+    @RequestMapping("/equipment/delete")
+    public String deleteEquipment(Integer equipmentid){
+        if(equipmentRepository.findByEquipmentid(equipmentid) != null){
+            equipmentRepository.delete(equipmentid);
+            eventEquipmentRepository.deleteByEquipmentid(equipmentid);
+            return "success";
+        }
+        return "false";
+    }
 }
